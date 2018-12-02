b0c5aa2d13fce999e4107156eef8fd1ab12ceb4d
Plisp <tianlinqu@gmail.com>
2018-12-02 14:13:34 +1100

    fixed update-src script

--- scripts/update-src.sh
+++ scripts/update-src.sh
@@ -3,10 +3,9 @@
 ## UPDATE SOURCES AND BUILD PROGRAMS IN /usr/src
 ## for my distro-hopping convenience
 mkdir -p /usr/local/src
-rm -f /usr/local/bin/*
+cd /usr/local/src
 
 # global (tagging)
-cd /usr/local/src
 if [ ! -d /usr/local/src/global ]; then
 	git clone https://github.com/harai/gnu-global.git
 fi
@@ -17,27 +16,23 @@ git pull
 make clean
 make -j3 --quiet
 make install -j8 --quiet
+cd ..
 
 # emacs
-cd /usr/local/src
-if [ ! -d emacs ]; then
-	git clone https://git.savannah.gnu.org/git/emacs.git
-fi
+#if [ ! -d emacs ]; then
+#	git clone https://git.savannah.gnu.org/git/emacs.git
+#fi
 
-cd emacs
-git pull
-./configure --quiet --without-toolkit-scroll-bars --without-dbus --without-gsettings --without-libsystemd \
-            --with-sound=alsa --with-mailutils --with-x-toolkit=athena --with-x
-make clean
-make -j3 --quiet
-make install -j8 --quiet
+#cd emacs
+#git pull
+#./configure --quiet --without-toolkit-scroll-bars --without-dbus --without-gsettings --without-libsystemd \
+#            --with-sound=alsa --with-mailutils --with-x-toolkit=athena --with-x
+#make clean
+#make -j3 --quiet
+#make install -j8 --quiet
+#cd ..
 
 # universal-ctags (maintained ctags)
-# don't want emacs-etags or its manual
-rm /usr/local/bin/ctags /usr/local/bin/etags
-rm /usr/local/share/man/man1/ctags.1.gz /usr/local/share/man/man1/etags.1.gz
-
-cd /usr/local/src
 if [ ! -d ctags ]; then
 	git clone https://github.com/universal-ctags/ctags.git
 fi
@@ -48,9 +43,9 @@ git pull
 make clean
 make -j3 --quiet
 make install -j8 --quiet
+cd ..
 
 # suckless terminal
-cd /usr/local/src
 if [ ! -d st ]; then
 	git clone https://github.com/Plisp/st.git
 fi
@@ -58,9 +53,9 @@ fi
 cd st
 git pull
 make clean install -j3 --quiet
+cd ..
 
 # ccls - c language server protocol
-cd /usr/local/src
 if [ ! -d ccls ]; then
 	git clone https://github.com/Maskray/ccls.git
 fi
@@ -69,3 +64,4 @@ cd ccls
 git pull
 cmake -H. -BRelease -DSYSTEM_CLANG=on -DUSE_SHARED_LLVM=on -DLLVM_ENABLE_RTTI=on
 cmake --build Release
+cd ..
